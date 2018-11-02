4ef68f36d5488678680c9a5d10de02770c7c38b5
sunmix07 <sunmix_07@bk.ru>
2018-11-02 23:57:02 +0300

    Update calendar.js

--- src/calendar.js
+++ src/calendar.js
@@ -1,67 +1,70 @@
-import moment from 'moment';
-import React, { Component } from 'react';
-import cx from 'classnames';
-import range from 'lodash/range';
-import chunk from 'lodash/chunk';
+var cx = require('classnames');
+var moment = require('moment');
+var React = require('react');
+var range = require('lodash/utility/range');
+var chunk = require('lodash/array/chunk');
 
-const Day = ({ i, w, d, className, ...props }) => {
-  const prevMonth = w === 0 && i > 7;
-  const nextMonth = w >= 4 && i <= 14;
-  const cls = cx({
-    'prev-month': prevMonth,
-    'next-month': nextMonth,
-    'current-day': !prevMonth && !nextMonth && i === d
-  });
-
-  return <td className={cls} {...props}>{i}</td>;
-};
-
-export default class Calendar extends Component {
-  selectDate = (i, w) => {
-    const prevMonth = w === 0 && i > 7;
-    const nextMonth = w >= 4 && i <= 14;
-    const m = this.props.moment;
-
-    if (prevMonth) m.subtract(1, 'month');
-    if (nextMonth) m.add(1, 'month');
-
-    m.date(i);
-
-    this.props.onChange(m);
-  };
-
-  prevMonth = e => {
-    e.preventDefault();
-    this.props.onChange(this.props.moment.subtract(1, 'month'));
-  };
-
-  nextMonth = e => {
-    e.preventDefault();
-    this.props.onChange(this.props.moment.add(1, 'month'));
-  };
+var Day = React.createClass({
+  displayName: 'Day',
 
   render() {
-    const m = this.props.moment;
-    const d = m.date();
-    const d1 = m.clone().subtract(1, 'month').endOf('month').date();
-    const d2 = m.clone().date(1).day();
-    const d3 = m.clone().endOf('month').date();
-    const days = [].concat(
-      range(d1 - d2 + 1, d1 + 1),
-      range(1, d3 + 1),
-      range(1, 42 - d3 - d2 + 1)
-    );
-    const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var i = this.props.i;
+    var w = this.props.w;
+    var prevMonth = (w === 0 && i > 7);
+    var nextMonth = (w >= 4 && i <= 14);
+    var cn = cx({
+      'prev-month': prevMonth,
+      'next-month': nextMonth,
+      'current-day': !prevMonth && !nextMonth && (i === this.props.d)
+    });
+
+    return <td className={cn} {... this.props}>{i}</td>;
+  }
+});
+
+module.exports = React.createClass({
+  displayName: 'Calendar',
+
+  render() {
+    var m = this.props.moment;
+    var d = m.date();
+    var d1 = m.clone().subtract(1, 'month').endOf('month').date();
+    var d2 = m.clone().date(1).day();
+    var d3 = m.clone().endOf('month').date();
+
+    // get short names for weekdays in current locale, example:
+    //   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    var weeks = moment.weekdaysShort();
+
+    // check if week starts with Monday
+    if (moment.localeData().firstDayOfWeek() == 1) {
+      // swap sunday with monday (weekday names)
+      weeks.push(weeks.shift());
+
+      if (d2 == 0)
+        d2 = 7; // convert to ISO day of the week (1..7)
+      var days = [].concat(
+        range(d1-d2+2, d1+1),
+        range(1, d3+1),
+        range(1, 42-d3-d2+2)
+      );
+    } else {
+      var days = [].concat(
+        range(d1-d2+1, d1+1),
+        range(1, d3+1),
+        range(1, 42-d3-d2+1)
+      );
+    }
 
     return (
       <div className={cx('m-calendar', this.props.className)}>
         <div className="toolbar">
           <button type="button" className="prev-month" onClick={this.prevMonth}>
-            <i className={this.props.prevMonthIcon} />
+            <i className="ion-ios-arrow-left"/>
           </button>
           <span className="current-date">{m.format('MMMM YYYY')}</span>
           <button type="button" className="next-month" onClick={this.nextMonth}>
-            <i className={this.props.nextMonthIcon} />
+            <i className="ion-ios-arrow-right"/>
           </button>
         </div>
 
@@ -73,22 +76,40 @@ export default class Calendar extends Component {
           </thead>
 
           <tbody>
-            {chunk(days, 7).map((row, w) =>
+            {chunk(days, 7).map((row, w) => (
               <tr key={w}>
-                {row.map(i =>
-                  <Day
-                    key={i}
-                    i={i}
-                    d={d}
-                    w={w}
-                    onClick={() => this.selectDate(i, w)}
+                {row.map((i) => (
+                  <Day key={i} i={i} d={d} w={w}
+                    onClick={this.selectDate.bind(null, i, w)}
                   />
-                )}
+                ))}
               </tr>
-            )}
+            ))}
           </tbody>
         </table>
       </div>
     );
+  },
+
+  selectDate(i, w) {
+    var prevMonth = (w === 0 && i > 7);
+    var nextMonth = (w >= 4 && i <= 14);
+    var m = this.props.moment;
+
+    m.date(i);
+    if(prevMonth) m.subtract(1, 'month');
+    if(nextMonth) m.add(1, 'month');
+
+    this.props.onChange(m);
+  },
+
+  prevMonth(e) {
+    e.preventDefault();
+    this.props.onChange(this.props.moment.subtract(1, 'month'));
+  },
+
+  nextMonth(e) {
+    e.preventDefault();
+    this.props.onChange(this.props.moment.add(1, 'month'));
   }
-}
+});
